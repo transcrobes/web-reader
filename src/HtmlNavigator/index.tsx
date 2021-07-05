bb414a3f7acebf5a98f251e023fe2c9b5e7161e1
kristojorg <kristo.jorgenson@gmail.com>
2021-07-05 18:26:32 +0200

    new D2Reader API

--- src/HtmlNavigator/index.tsx
+++ src/HtmlNavigator/index.tsx
@@ -1,11 +1,8 @@
-import D2Reader from '@d-i-t-a/reader';
+import D2Reader, { ReadingPosition, Locator } from '@d-i-t-a/reader';
 import Navigator, { NavigatorArguments } from '../Navigator';
-import { ReadingPosition } from '@d-i-t-a/reader/dist/model/Locator';
-import '@d-i-t-a/reader/dist/reader.css';
 import EpubContent from './HtmlNavigatorContent';
 import { WebpubManifest } from '../types';
 import { fetchJson } from '../utils/fetch';
-import { Link } from '@d-i-t-a/reader/dist/model/Publication';
 import { mutating } from '../decorators';
 
 /**
@@ -15,8 +12,11 @@ import { mutating } from '../decorators';
 export default class HtmlNavigator extends Navigator {
   static Content = EpubContent;
 
-  private constructor(didMutate: () => void) {
+  private readonly reader: D2Reader;
+
+  private constructor(didMutate: () => void, reader: D2Reader) {
     super(didMutate);
+    this.reader = reader;
   }
 
   static async init({
@@ -24,44 +24,14 @@ export default class HtmlNavigator extends Navigator {
     didMutate,
   }: NavigatorArguments): Promise<HtmlNavigator> {
     const url = new URL(webpubManifestUrl);
-    const reader = await D2Reader.load({
+    const reader = await D2Reader.build({
       url,
       injectables: injectables as any,
-
-      // all of these were required
-      userSettings: {
-        verticalScroll: 'readium-scroll-off',
-      },
-      initialAnnotations: undefined,
-      lastReadingPosition: undefined,
-      upLinkUrl: undefined,
-      material: {
-        settings: {
-          fontOverride: false,
-          advancedSettings: false,
-          pageMargins: false,
-          lineHeight: false,
-        },
-      },
-      rights: {
-        autoGeneratePositions: false,
-      },
-      tts: undefined,
-      search: { color: 'red', current: 'blah' },
-      annotations: { initialAnnotationColor: 'blue' },
-      highlighter: { selectionMenuItems: [] },
-      useLocalStorage: false,
-      attributes: { margin: 2 },
-      // TODO: Fix this any assertion
-    } as any);
-
-    const navigator = new HtmlNavigator(didMutate);
+      injectablesFixed: [],
+    });
+    const navigator = new HtmlNavigator(didMutate, reader);
     return navigator;
   }
-
-  // get isScrolling(): boolean {
-  //   return D2Reader.isScroll();
-  // }
 
   get readingProgression(): ReadingPosition {
     return {
@@ -81,33 +51,33 @@ export default class HtmlNavigator extends Navigator {
     // return D2Reader.currentLocator;
   }
 
-  async goTo(link: Link) {
+  goTo = async (locator: Locator) => {
     try {
-      await D2Reader.goTo(link);
+      await this.reader.goTo(locator);
       return true;
     } catch (e) {
       console.error(e);
       return false;
     }
-  }
-  async goForward() {
+  };
+  goForward = async () => {
     try {
-      await D2Reader.nextPage();
+      await this.reader.nextPage();
       return true;
     } catch (e) {
       console.error(e);
       return false;
     }
-  }
-  async goBackward() {
+  };
+  goBackward = async () => {
     try {
-      await D2Reader.previousPage();
+      await this.reader.previousPage();
       return true;
     } catch (e) {
       console.error(e);
       return false;
     }
-  }
+  };
   async goLeft() {
     return this.goBackward();
   }
@@ -118,25 +88,19 @@ export default class HtmlNavigator extends Navigator {
   // settings
   @mutating
   scroll() {
-    D2Reader.scroll(true);
+    this.reader.scroll(true);
   }
-
   @mutating
   paginate() {
-    D2Reader.scroll(false);
-  }
-  get isScroll() {
-    const settings = D2Reader.currentSettings();
-    console.log(settings);
-    return false;
+    this.reader.scroll(false);
   }
   get isScrolling() {
-    return false;
+    return this.reader.currentSettings.verticalScroll;
   }
-  toggleScroll() {
+  toggleScroll = () => {
     console.warn('toggle scroll not implemented');
     return;
-  }
+  };
 
   static async fetchManifest(url: string): Promise<WebpubManifest> {
     const manifest: WebpubManifest = await fetchJson(url);
@@ -159,47 +123,5 @@ const injectables = [
     type: 'style',
     url: 'http://localhost:1234/viewer/readium-css/ReadiumCSS-after.css',
     r2after: true,
-  },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/pagebreak/pagebreak.css',
-    r2after: true,
-  },
-  // { type: 'style', url: 'http://localhost:1234/viewer/readium-css/neon-after.css', r2after: true, appearance: 'neon' },
-  {
-    type: 'script',
-    url:
-      'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML&latest',
-  },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/fonts/opendyslexic/opendyslexic.css',
-    fontFamily: 'opendyslexic',
-    systemFont: false,
-  },
-  { type: 'style', fontFamily: 'Courier', systemFont: true },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/click/click.js',
-  // },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/footnotes/footnotes.js',
-  // },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/footnotes/footnotes.css',
-  },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/glossary/glossary.js',
-  // },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/glossary/glossary.css',
-  },
-  {
-    type: 'style',
-    url: 'http://localhost:1234/viewer/injectables/style/style.css',
   },
 ];
